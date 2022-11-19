d004b1d9511d02e7fc3b18b74817b2750c39713a
Rodionovmax <mrodionov@connectrn.com>
2022-11-19 17:21:56 -0500

    7. Architecture skeleton and xml layouts

--- app/src/main/java/com/rodionovmax/runningapp/ui/run/RunFragment.kt
+++ app/src/main/java/com/rodionovmax/runningapp/ui/run/RunFragment.kt
@@ -1,30 +1,30 @@
-package com.rodionovmax.runningapp
+package com.rodionovmax.runningapp.ui.run
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import com.rodionovmax.runningapp.databinding.FragmentFirstBinding
+import com.rodionovmax.runningapp.R
+import com.rodionovmax.runningapp.databinding.FragmentRunBinding
+import com.rodionovmax.runningapp.ui.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
-class FirstFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+@AndroidEntryPoint
+class RunFragment : Fragment() {
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    private var _binding: FragmentRunBinding? = null
     private val binding get() = _binding!!
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentRunBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -32,9 +32,7 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+
     }
 
     override fun onDestroyView() {
