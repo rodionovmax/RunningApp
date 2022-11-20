0142b36c61118d301e28a8ab19bbdc7be86d55d1
Rodionovmax <mrodionov@connectrn.com>
2022-11-19 23:10:38 -0500

    9. handling maps lifecycle

--- app/src/main/java/com/rodionovmax/runningapp/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/rodionovmax/runningapp/ui/settings/SettingsFragment.kt
@@ -1,27 +1,24 @@
 package com.rodionovmax.runningapp.ui.settings
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.rodionovmax.runningapp.R
+import androidx.fragment.app.Fragment
+import com.rodionovmax.runningapp.databinding.FragmentSettingsBinding
 
 
 class SettingsFragment : Fragment() {
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private var _binding: FragmentSettingsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+    ): View {
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
 
--- app/src/main/java/com/rodionovmax/runningapp/ui/tracking/TrackingFragment.kt
+++ app/src/main/java/com/rodionovmax/runningapp/ui/tracking/TrackingFragment.kt
@@ -1,12 +1,13 @@
 package com.rodionovmax.runningapp.ui.tracking
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.rodionovmax.runningapp.R
+import com.google.android.gms.maps.GoogleMap
+import com.rodionovmax.runningapp.databinding.FragmentTrackingBinding
 import com.rodionovmax.runningapp.ui.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,18 +15,61 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class TrackingFragment : Fragment() {
 
+    private var _binding: FragmentTrackingBinding? = null
+    private val binding get() = _binding!!
     private val viewModel: MainViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var map: GoogleMap? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tracking, container, false)
+    ): View {
+        _binding = FragmentTrackingBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.mapView.onCreate(savedInstanceState)
+        binding.mapView.getMapAsync {
+            map = it
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapView.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapView.onStop()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView.onPause()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView.onLowMemory()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.mapView.onSaveInstanceState(outState)
+    }
+
+    /*override fun onDestroy() {
+        super.onDestroy()
+        binding.mapView.onDestroy()
+    }*/
 
 }
